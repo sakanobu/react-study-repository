e036a2ac60164aa5cb26dc76d19ff6c786416eec
sakanobu <grapejuice100percent@gmail.com>
2021-05-22 12:11:08 +0900

    "extends"の"airbnb/hooks"と"plugin:react-hooks/recommended"を消去､"exteds"のjestの設定を"plugin:jest/all"に､rulesのjestとreactとreact-hooksの設定を追加及び変更､またそれらの設定に基づいて各ファイルをフォーマット

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from 'App';
 
-test('App', () => {
-  render(<App />);
+describe('App', () => {
+  it('Count Button', () => {
+    render(<App />);
 
-  screen.debug();
+    // eslint-disable-next-line testing-library/no-debug
+    screen.debug();
 
-  // https://github.com/testing-library/jest-dom
+    // https://github.com/testing-library/jest-dom
 
-  expect(screen.getByRole('button')).toHaveAttribute('type', 'button');
+    expect(screen.getByRole('button')).toHaveAttribute('type', 'button');
 
-  expect(screen.getByRole('button')).toHaveTextContent('カウント');
+    expect(screen.getByRole('button')).toHaveTextContent('カウント');
 
-  // expect(screen.getByText(/0/)).toHaveTextContent('0');
+    // expect(screen.getByText(/0/)).toHaveTextContent('0');
 
-  const div0 = screen.getByText(/0/);
+    const div0 = screen.getByText(/0/);
 
-  expect(div0).toHaveTextContent('0');
+    expect(div0).toHaveTextContent('0');
+  });
 });
